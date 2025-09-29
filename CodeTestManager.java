651f9bffada016055a4c647fe55b1e5898a3e3d8
michaelellischaffin-cmyk <michael.ellis.chaffin@gmail.com>
2025-09-29 17:01:01 +0100

    Add files via upload

---
+++ CodeTestManager.java
@@ -0,0 +1,12 @@
+package com.codetest.codetestmanager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class CodeTestManager {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeTestManager .class, args);
+
+    }
+}
